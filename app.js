7bb8aed526b545dc3402807a717cc9d7f43c4c62
Pedro Moreno <pm305567@gmail.com>
2025-04-15 16:36:51 -0300

    feat: get all users endpoint

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require("express");
+import express from "express";
 const app = express();
+app.use(express.json());
 const port = 3000;
-
+const usuario = [
+  { id: 1, nome: "João", email: "joaodugrau@gmail.com" },
+  { id: 2, nome: "Ana", email: "anabanana@gmail.com" },
+];
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Bem vindo a minha API!");
 });
-
+app.get("/usuarios", (req, res) => {
+  res.send(usuario);
+});
+app.post("/criarUsuario", (req, res) => {
+  const { nome } = req.body;
+  res.send(nome);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
